7b1bfb81be90791887ca813badd761023dc94e85
vishesh <visheshbangotra@gmail.com>
2017-04-28 17:22:04 +0530

    event listener api working

--- listener/views.py
+++ listener/views.py
@@ -1,20 +1,18 @@
 # App Imports
-import time
+import json
 import models
-import calendar
 
 # Django Imports
 from django.http import JsonResponse
-from django.views.generic import ListView
-from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 
 
 @csrf_exempt
 def listener(request):
     if request.method == 'POST':
-        response = JsonResponse(data={'success': True})
+        json_data = json.loads(request.body)
+        event = models.Event.create_from_api(json_data)
+        response = JsonResponse(data={'success': True, 'event': {'id': event.pk, 'userid': event.user_id}})
     else:
         response = JsonResponse(data={'success': False})
     return response
